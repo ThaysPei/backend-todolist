0e22001cdf943b1a2f98aaa7d7aae819037b0fbb
Thays Peixoto <thabysilva12@gmail.com>
2026-09-07 13:31:01 -0300

    feat: cria estrutura inicial (app, server, router)

---
+++ backend/src/router.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+
+const router = express.Router()
+
+
+//requisiçoes
+router.get('/', (req,res) => res.status(200).send("o router está funcionando"));
+
+
+
+module.exports = router;
+
