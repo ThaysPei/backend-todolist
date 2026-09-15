1882c9edbb7000436947298ce49d80dabab10ed5
Thays Peixoto <thabysilva12@gmail.com>
2026-09-15 17:36:23 -0300

    feat: atualiza correção de bug

---
+++ backend/src/models/tasksModel.js
@@ -0,0 +1,10 @@
+const connection = require("./connection"); //conexao com banco de dados
+const getAll = async () => {
+  const tasks = await connection.execute("SELECT * FROM tasks"); // função de busca GET
+  return tasks;
+};
+
+module.exports = {
+  getAll,
+};
+// objeto que recebe funcao de busca
