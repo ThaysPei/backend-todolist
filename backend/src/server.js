65394a28852d37158ec73700393c0069f8ef8a8a
Thays Peixoto <thabysilva12@gmail.com>
2026-09-15 17:37:00 -0300

    feat: correção de bug

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,6 @@
-const app = require('./app'); 
+const app = require("./app");
 
 const PORT = process.env.PORT || 3333;
-require('dotenv').config();
-
+require("dotenv").config();
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`)); //aqui roda o server
