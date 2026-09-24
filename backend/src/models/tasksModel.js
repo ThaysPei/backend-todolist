714a9b4f00084cb97c59e859e5e09a5eabf71986
Thays Peixoto <thabysilva12@gmail.com>
2026-09-24 16:18:16 -0300

    feat: cria função de cadastro de tasks

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -5,7 +5,18 @@ const getAll = async () => {
   return tasks;
 };
 
+
+const createTasks = async (task) => {
+
+const { title } = task;
+const query = 'INSERT INTO tasks(title, status, created_at) VALUES (?, ?, ?)';
+const createdTasks = await connection.execute(query, [title, 'pendente', 'asdasd'])
+
+}
+
+
 module.exports = {
   getAll,
+  createTasks,
 };
 // objeto que recebe funcao de busca
