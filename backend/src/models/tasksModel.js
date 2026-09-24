051294aaf4a9724101fa2a07a33960593de6e364
Thays Peixoto <thabysilva12@gmail.com>
2026-09-24 13:27:07 -0300

    feat: torna a variável tasks objeto de retorno de requisição

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -1,6 +1,7 @@
-const connection = require("./connection"); //conexao com banco de dados
+const connection = require("./connection"); // conexão com banco de dados
+
 const getAll = async () => {
-  const tasks = await connection.execute("SELECT * FROM tasks"); // função de busca GET
+  const [tasks] = await connection.execute("SELECT * FROM tasks"); // função de busca GET
   return tasks;
 };
 
